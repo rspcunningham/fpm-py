0be5dbd2e81b03f9199f38ef5f804cf96ba126b3
Robin Cunningham <79066805+rspcunningham@users.noreply.github.com>
2024-08-14 11:34:41 -0400

    fix point form

--- fpm_py/__init__.py
+++ fpm_py/__init__.py
@@ -4,11 +4,11 @@ Fourier ptychography package for Python.
 This package provides tools for performing Fourier ptychography microscopy (FPM) reconstruction in Python.
 
 The package is organized into the following modules:
-    `algorithm`: Contains the main reconstruction algorithm.
-    `data`: Contains the `ImageCapture` and `ImageSeries` classes for storing image data.
-    `iteration_terminators`: Contains functions that determine when to stop the iteration process.
-    `optimizers`: Contains the optimizers used in the FPM reconstruction process.
-    `utils`: Contains utility functions used throughout the package.
+- algorithm`: Contains the main reconstruction algorithm.
+- data`: Contains the `ImageCapture` and `ImageSeries` classes for storing image data.
+- iteration_terminators`: Contains functions that determine when to stop the iteration process.
+- optimizers`: Contains the optimizers used in the FPM reconstruction process.
+- utils`: Contains utility functions used throughout the package.
 
 """
 
