7ad547ab5baac35bcd02bc46525f2787a3d80114
Robin Cunningham <79066805+rspcunningham@users.noreply.github.com>
2025-11-13 21:54:28 -0500

    nice

--- src/ptych/utils.py
+++ src/ptych/utils.py
@@ -10,6 +10,14 @@ def get_default_device():
 def obj_to_amp(obj: torch.Tensor):
     return (torch.abs(obj) / torch.max(torch.abs(obj))).cpu()
 
+def normalize_captures(captures: torch.Tensor, method: str = "global"):
+    if method == "global":
+        return (captures - captures.min()) / (captures.max() - captures.min() + 1e-8)
+    if method == "each":
+        return (captures - captures.min(dim=1, keepdim=True).values) / \
+                          (captures.max(dim=1, keepdim=True).values - captures.min(dim=1, keepdim=True).values + 1e-8)
+
+    raise ValueError(f"Invalid normalization method: {method}, must be 'global' or 'each'")
 
 def check_range(tensor: torch.Tensor, min_val: float, max_val: float, name: str | None = None):
     if not torch.all(tensor >= min_val) or not torch.all(tensor <= max_val):
